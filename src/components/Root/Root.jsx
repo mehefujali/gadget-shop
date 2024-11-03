6a1e3c1c6b2c75c8349b9eb01d97d1ed4cec2cea
Mehefuj Ali <mehefujalim@gmail.com>
2024-11-03 23:24:31 +0530

    Nav bar created

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,9 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Nav from "../Nav/Nav";
 
 const Root = () => {
       return (
-            <nav>
+            <div >
+                  <header>
+                        <Nav></Nav>
+                  </header>
+                  <main className=" container mx-auto">
+                        <Outlet></Outlet>
+                  </main>
+                  <footer>
 
-            </nav>
+                  </footer>
+            </div>
       );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {
   createBrowserRouter,
+  Navigate,
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/Root/Root';
+import Home from './components/Home/Home';
 
 
 
@@ -14,6 +16,16 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to='/home' replace> </Navigate>
+      },
+      {
+        path: '/home',
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
