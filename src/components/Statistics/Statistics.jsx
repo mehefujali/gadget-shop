ef51c61e0b55128b43d74286aecae5753cf47404
Mehefuj Ali <mehefujalim@gmail.com>
2024-11-06 16:09:51 +0530

    no data

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,8 +5,9 @@ const Statistics = () => {
       useDocumentTitle("statistics")
       return (
             <div>
-                  <div className="container mx-auto">
-                        <h1>statistics</h1>
+                  <div className="container mx-auto flex flex-col text-center items-center justify-center border-2 min-h-[30vh] rounded-xl border-gray-300">
+                        <img className=" opacity-30 max-w-32" src="https://cdn-icons-png.flaticon.com/512/7466/7466140.png" alt="" />
+                        <h1 className=" text-gray-400 text-3xl font-thin">No Data </h1>
                   </div>
             </div>
       );
